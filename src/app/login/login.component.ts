adc1c5489e3ddbdcb1132d1006489cbaec24d209
MARIA <vmjmmaria2000@gmail.com>
2023-05-30 12:11:20 +0530

    new update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,23 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit  {
 
   data = "Happy Banking With Us!!"
   pdata = "Enter Account No"
 
-  login(a:any){
-    console.log(a.value);
+  serviceData:any
+
+  constructor(private rout:Router, private ds:DataService) { }
+
+  ngOnInit(): void{
+    this.serviceData = this.ds.sdata
+    console.log(this.serviceData);
+    this.ds.smethod()
     
-    alert("Login Success")
   }
 
-  acnoChange(event:any){
-    console.log(event.target.value);
+  login(){
+    // console.log("Login Success");
     
+    // alert("Login Success")
+    this.rout.navigateByUrl('home')
   }
+
+  // acnoChange(event:any){
+  //   console.log(event.target.value);
+    
+  // }
 }
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class RegistrationComponent {
 
+
+acno:any
+uname:any
+psw:any
+cpsw:any
+
+signup(){
+  // console.log(this.acno);
+  // console.log(this.uname);
+  // console.log(this.psw);
+  // console.log(this.cpsw);
+
+//another method:
+  var acno = this.acno
+  var uname = this.uname
+  var pswd = this.psw
+  var cpswd = this.cpsw
+
+  console.log(acno);
+  console.log(uname);
+  console.log(pswd);
+  console.log(cpswd);
+
+}
+
 }
